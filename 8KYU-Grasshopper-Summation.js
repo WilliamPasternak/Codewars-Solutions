a82b7a538e442ab721345304801174d7c50a5208
William Pasternak <wjp09@yahoo.com>
2022-03-20 13:47:17 -0400

    Challenge Instructions Included

--- 8KYU-Grasshopper-Summation.js
+++ 8KYU-Grasshopper-Summation.js
@@ -1,5 +1,7 @@
-//8 KYU Grasshopper - Summation 
-//8 KYU Grasshopper - Summation 
+/*8 KYU Grasshopper - Summation 
+Summation
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+*/
 
 var summation = function (num) {
   let sum = 0;
