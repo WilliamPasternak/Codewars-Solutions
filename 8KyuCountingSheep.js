dd14ff7f9467abd0e97d2f01ab5494f2bbb9a7fb
William Pasternak <wjp09@yahoo.com>
2022-03-22 11:29:34 -0400

    Initial Commit

---
+++ 8KyuCountingSheep.js
@@ -0,0 +1,19 @@
+
+/*
+8 kyu
+Counting sheep...
+ Consider an array/list of sheep where some sheep may be missing from their place. 
+We need a function that counts the number of sheep present in the array (true means present).
+*/
+
+
+function countSheeps(arrayOfSheep) {
+  let counter = 0;
+  arrayOfSheep.forEach(sheep => {
+    if(sheep === true){
+      counter++;
+    }
+  })
+  return counter;
+  
+}
