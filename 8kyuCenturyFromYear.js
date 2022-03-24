5ab35954e42acd89f73a627391439b171856e4c6
William Pasternak <wjp09@yahoo.com>
2022-03-23 20:18:31 -0400

    initial

---
+++ 8kyuCenturyFromYear.js
@@ -0,0 +1,26 @@
+/* 
+8 kyu
+Century From Year
+Introduction
+The first century spans from the year 1 up to and including the year 100, 
+the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+*/
+
+// function century(year) {
+//   if (Number.isInteger(year/100)){
+//     return year / 100
+//   }
+//   else {
+//     return Math.floor((year / 100))+1
+//   }
+
+// }
+
+// Number.isInteger(year / 100)? year / 100:  Math.floor((year / 100))+1
+
+function century(year) {
+    return Math.ceil(year / 100)
+  }
