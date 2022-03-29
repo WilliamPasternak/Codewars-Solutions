cc5c0a7122cf65ab546a610a3f08b472f33b6217
William Pasternak <wjp09@yahoo.com>
2022-03-29 13:06:00 -0400

    initial

--- 8KyuSumMixedArray.js
+++ 8KyuSumMixedArray.js
@@ -1,5 +1,6 @@
 /* 
 8 kyu
+3/29
 Sum Mixed Array
 Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
@@ -8,4 +9,4 @@ Given an array of integers as strings and numbers, return the sum of the array v
 function sumMix(x){
   let allNums = x.map(x => +x) // Turns all elements to numbers
 return allNums.reduce((a,b) => a+b,0)   // Sums all numbers
-}
+}  
