ba68d6e1f231122502a8c192ef97fae513d6f3c0
William Pasternak <wjp09@yahoo.com>
2022-04-01 13:21:48 -0400

    initial

---
+++ 8KyuGrasshopperPersonalizedMessage.js
@@ -0,0 +1,10 @@
+/*
+8 kyu
+Grasshopper - Personalized Message
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+*/
+
+
+function greet (name, owner) {
+return name === owner? "Hello boss" : "Hello guest"
+}
