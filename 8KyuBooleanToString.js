787b9ff3675f1856e33c10b4ed066e28c048f311
William Pasternak <wjp09@yahoo.com>
2022-04-01 13:01:14 -0400

    initial

---
+++ 8KyuBooleanToString.js
@@ -0,0 +1,16 @@
+/* 8 kyu
+Convert a Boolean to a String
+Implement a function which convert the given boolean value into its string representation.
+*/
+
+function booleanToString(b){
+  return b ? 'true' : 'false'
+  }
+  
+//   if(b === true){
+//     return "true"
+//   }
+//   else {
+//     return "false"
+//   }
+// }
