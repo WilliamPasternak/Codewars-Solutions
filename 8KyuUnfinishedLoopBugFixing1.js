bd0ee96d60b8b84f4504b896b01dfe25f8ec8a6d
William Pasternak <wjp09@yahoo.com>
2022-04-04 10:30:39 -0400

    initial

---
+++ 8KyuUnfinishedLoopBugFixing1.js
@@ -0,0 +1,13 @@
+/*
+8 kyu
+Unfinished Loop - Bug Fixing #1
+*/
+function createArray(number){
+  var newArray = [];
+  
+  for(var i = 1; i <= number; i++){
+    newArray.push(i);
+  }
+  
+  return newArray;
+}
