0bf75f4ea2c118ede8ea6d5699366f93c9d6a0b1
William Pasternak <wjp09@yahoo.com>
2022-04-10 13:49:48 -0400

    initial

---
+++ 8KyuReversedSequence.js
@@ -0,0 +1,15 @@
+/* 
+8 kyu
+Reversed sequence
+Build a function that returns an array of integers from n to 1 where n>0.
+*/
+
+
+const reverseSeq = n => {
+  let newArr = []
+  for (let i = n; i > 0 ; i--){
+   newArr.push(i)
+  }
+  
+  return newArr;
+};
