22b9c0390704fc08636a446210a1ec0ad9b061ea
William Pasternak <wjp09@yahoo.com>
2022-04-13 14:31:54 -0400

    initial

---
+++ 8KyuTerminalMove.js
@@ -0,0 +1,9 @@
+/* 
+8KYU
+Grasshopper - Terminal game move function
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+*/
+
+function move (position, roll) {
+  return position + (roll * 2)
+}
