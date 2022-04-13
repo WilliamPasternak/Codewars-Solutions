c95e57e74ea0d29af9f5a0494873c9a6a145ccf9
William Pasternak <wjp09@yahoo.com>
2022-04-13 10:57:39 -0400

    initial

---
+++ 8KyuSetAlarm.js
@@ -0,0 +1,16 @@
+/*8 kyu
+L1: Set Alarm 
+The function should return true if you are employed and not on vacation
+*/
+
+// function setAlarm(employed, vacation){
+// if(employed === true && vacation === false){
+//   return true
+// }
+//   return false
+  
+// }
+
+function setAlarm(employed, vacation){
+  return employed === true && vacation === false ? true:false
+  }
