e6c5eddfbb2b1e4a98ceb99484e6cdc1b24e6925
William Pasternak <wjp09@yahoo.com>
2022-04-23 18:55:18 -0400

    initial

--- 8Kyu42322.js
+++ 8Kyu42322.js
@@ -24,3 +24,15 @@ function apple(x){
   return x * x > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
 }
 
+/*8 kyu
+Find Multiples of a Number */
+
+function findMultiples(integer, limit) {
+  const arr = []
+  for (let i = 1; i * integer <= limit; i++){
+    arr.push(integer*i)
+  }
+  return arr
+ 
+}
+
