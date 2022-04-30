d6f49f2d413471bbe81ec5005d383182b9447393
William Pasternak <wjp09@yahoo.com>
2022-04-30 13:09:29 -0400

    initial

--- 4:30.js
+++ 4:30.js
@@ -1,6 +1,11 @@
-4/30.js
+// 4/30/22
 
 //Reverse Meerkat
 function fixTheMeerkat(arr) {
  return arr.reverse()
+}
+
+//8 kyu Find numbers which are divisible by given number
+function divisibleBy(numbers, divisor){
+return numbers.filter(x => x % divisor === 0)
 }
