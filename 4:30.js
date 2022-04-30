2d353597e44d74c6fe05145bb2a987b3c445d6c9
William Pasternak <wjp09@yahoo.com>
2022-04-30 13:35:06 -0400

    initial

--- 4:30.js
+++ 4:30.js
@@ -24,3 +24,14 @@ function stringy(size) {
   }
   return binary
 }
+
+//8 kyu Grasshopper - If/else syntax debug
+
+function checkAlive (health) {
+  if (health > 0) {
+    return true
+  } 
+  else{
+    return false
+  }
+}
