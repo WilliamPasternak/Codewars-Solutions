8adad602f216d32ca7b8e9e6f9b4e1488d42778f
William Pasternak <wjp09@yahoo.com>
2022-05-01 10:35:58 -0400

    initial

---
+++ 5:1:22.js
@@ -0,0 +1,20 @@
+5/1/22.js
+/*8 kyu 101 Dalmatians - squash the bugs, not the dogs!*/
+
+function howManyDalmatians(number){
+  if (number <= 10){
+    return "Hardly any"
+  }
+  if (number <= 50){
+    return "More than a handful!"
+  }
+  if (number === 101){
+    return  '101 DALMATIANS!!!'
+  }
+ if (number){
+    return "Woah that's a lot of dogs!"
+  }
+  
+  }
+  
+  
