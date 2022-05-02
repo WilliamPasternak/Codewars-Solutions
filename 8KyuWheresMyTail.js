cb6b25ffaafe27f17f09bdd6c9732bdb35392247
William Pasternak <wjp09@yahoo.com>
2022-05-02 05:37:12 -0400

    initial

---
+++ 8KyuWheresMyTail.js
@@ -0,0 +1,13 @@
+/*8 kyu
+Is this my tail? */
+function correctTail(bod, tail){
+   let sub = bod.substr(bod.length-(tail.length))
+  
+  if (sub === tail) 
+    return true
+  
+  else 
+    return false
+}
+  
+ 
