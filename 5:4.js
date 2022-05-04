cac5bce7bc9891f34e91e706b8ba280f67441399
William Pasternak <wjp09@yahoo.com>
2022-05-04 12:09:52 -0500

    initial

--- 5:4.js
+++ 5:4.js
@@ -15,3 +15,26 @@ function sumTwoSmallestNumbers(numbers) {
   let west = walk.filter(x => x === 'w').length
   return walk.length === 10 && north === south && east === west
 }
+
+/* 7 kyu Sum of odd numbers*/
+function rowSumOddNumbers(n) {
+  if(n <= 1) return 1
+  let startingNum = 1
+  
+  // Sets first number in row
+for(let i = 1; i < n ;i++){
+    startingNum = i * n + 1
+ }
+  // Calculates sum of row
+let currentTotal = startingNum
+let solution = 0
+  
+  // Add 2 to Current total. Then add current total to solution.
+for(let i = 1; i < n ;i++){
+  currentTotal += 2 
+  solution += currentTotal 
+}
+  
+  return solution + startingNum
+}
+
