816d474661d85e832a50f120e7d8d851cec6cd32
William Pasternak <wjp09@yahoo.com>
2022-05-04 09:34:36 -0500

    completed 7Kyu

--- 5:4.js
+++ 5:4.js
@@ -1,2 +1,10 @@
 /* 8Kyu Add item to array*/
 websites[0]="codewars";
+
+/* 7 kyu Sum of two lowest positive integers */
+function sumTwoSmallestNumbers(numbers) {  
+  numbers.sort((a,b) => a - b)
+  return numbers[0] + numbers[1]
+}
+
+/* 6KYU */
