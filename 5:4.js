1f20e4155f8329b96bed90d5f3cf8ccce9e73c39
William Pasternak <wjp09@yahoo.com>
2022-05-04 10:01:39 -0500

    initial

--- 5:4.js
+++ 5:4.js
@@ -7,4 +7,11 @@ function sumTwoSmallestNumbers(numbers) {
   return numbers[0] + numbers[1]
 }
 
-/* 6KYU */
+/* 6 kyu Take a Ten Minutes Walk */
+  function isValidWalk(walk) {
+  let north = walk.filter(x => x === 'n').length
+  let south = walk.filter(x => x === 's').length
+  let east = walk.filter(x => x === 'e').length
+  let west = walk.filter(x => x === 'w').length
+  return walk.length === 10 && north === south && east === west
+}
