8dc9655ca307ab87668a29c70938b4bbd3308963
William Pasternak <wjp09@yahoo.com>
2022-05-19 12:10:24 -0400

    Last Challenge: Binary To Decimal

---
+++ 1. 8KYU/Fundamentals/Binary to Decimal.js
@@ -0,0 +1,10 @@
+//Binary to Decimal
+//Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// Function Declaration
+function binToDec(bin){
+  return parseInt(bin,2)
+}
+
+// Refactored Code
+let binToDec = bin => parseInt(bin,2)
