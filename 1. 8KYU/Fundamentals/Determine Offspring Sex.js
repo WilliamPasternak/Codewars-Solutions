e364d7eef10b377487c19381a288411d7d76a6e0
William Pasternak <wjp09@yahoo.com>
2022-05-22 12:32:40 -0400

    Refactored: Determine Offspring Sex

--- 1. 8KYU/Fundamentals/Determine Offspring Sex.js
+++ 1. 8KYU/Fundamentals/Determine Offspring Sex.js
@@ -5,3 +5,8 @@ Determine offspring sex based on genes XX and XY chromosomes
 function chromosomeCheck(sperm) {
   return sperm === 'XX' ? "Congratulations! You're going to have a daughter." : "Congratulations! You're going to have a son."
 }
+
+// Refactored:
+function chromosomeCheck(sperm) {
+  return `Congratulations! You're going to have a ${sperm === 'XY' ? 'son' : 'daughter'}.`
+}
