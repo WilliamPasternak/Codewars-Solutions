c8518a355c124c396582d190c95f8eaaa313febf
William Pasternak <wjp09@yahoo.com>
2022-05-29 09:43:31 -0400

    Last Challenge: Define A Card Suit

---
+++ 1. 8KYU/Fundamentals/Define a card suit.js
@@ -0,0 +1,9 @@
+Define a card suit
+You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+function defineSuit(card) {
+ if(card[card.length - 1] === '♣') return 'clubs'
+ else if(card[card.length - 1] === '♦') return  'diamonds'
+ else if(card[card.length - 1] === '♥') return  'hearts'
+ else if(card[card.length - 1] === '♠') return  'spades'
+ } 
