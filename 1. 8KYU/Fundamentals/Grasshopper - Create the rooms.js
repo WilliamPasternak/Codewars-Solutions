ccdc65c36e8a13419b00870414ebe88761ff3e2a
William Pasternak <wjp09@yahoo.com>
2022-06-20 19:58:49 -0400

    Last Challenge: Create The Rooms

---
+++ 1. 8KYU/Fundamentals/Grasshopper - Create the rooms.js
@@ -0,0 +1,13 @@
+// Grasshopper - Create the rooms
+var rooms = {
+  
+  roomOne: {
+    name: 'Will', description: 'Description', completed: false}
+  ,
+    roomTwo: {
+    name: 'Will', description: 'Description', completed: false}
+  ,
+    roomThree: {
+    name: 'Will', description: 'Description', completed: false}
+  }
+  
