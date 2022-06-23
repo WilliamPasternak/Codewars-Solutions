1d2dd17b7386a0980cf49c81011409f65109844b
William Pasternak <wjp09@yahoo.com>
2022-06-22 21:57:23 -0400

    Last Challenge: Complementary DNA

---
+++ 2. 7KYU/Fundamentals/Complementary DNA.js
@@ -0,0 +1,20 @@
+Complementary DNA
+function DNAStrand(dna){
+  let dnaArray = dna.split('')
+  
+  for(let i = 0; i < dnaArray.length; i++){
+    if(dnaArray[i] === 'A'){
+      dnaArray[i] = 'T'
+    }
+    else if(dnaArray[i] === 'T'){
+      dnaArray[i]= 'A'
+    }
+     else if(dnaArray[i] === 'C'){
+      dnaArray[i]= 'G'
+    }
+     else if(dnaArray[i] === 'G'){
+      dnaArray[i]= 'C'
+    }
+  } 
+  return dnaArray.join('')
+}
