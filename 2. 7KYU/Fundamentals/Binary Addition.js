5fd455bf853cef8e2d7c576617a2100048ce8d25
William Pasternak <wjp09@yahoo.com>
2022-07-11 21:25:14 -0400

    Last Challenge: Binary Addition

---
+++ 2. 7KYU/Fundamentals/Binary Addition.js
@@ -0,0 +1,12 @@
+Binary Addition
+// As a traditional function
+/*
+function addBinary(a,b) {
+  return (a + b).toString(2)
+}
+*/
+
+// As a function Expression
+
+let addBinary = (a,b) => (a + b).toString(2)
+
