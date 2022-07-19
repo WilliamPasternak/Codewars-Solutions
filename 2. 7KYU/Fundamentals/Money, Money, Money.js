324a93060acfa31c5995b0c5b9032f0e86fea800
William Pasternak <wjp09@yahoo.com>
2022-07-19 16:47:45 -0400

    Last Challenge: Money, Money, Money

---
+++ 2. 7KYU/Fundamentals/Money, Money, Money.js
@@ -0,0 +1,9 @@
+Money, Money, Money
+function calculateYears(principal, interest, tax, desired) {
+  let years = 0
+  while( principal < desired ){
+  years++
+  principal += ((principal * interest) - ((principal * interest) * tax))
+}
+  return years
+}
