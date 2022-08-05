8d666c75e8f4ea182475147be0bae953f829de94
William Pasternak <wjp09@yahoo.com>
2022-08-05 10:14:28 -0400

    Last Challenge: FizzBuzz!

---
+++ 2. 7KYU/Fundamentals/Fizz Buzz.js
@@ -0,0 +1,19 @@
+Fizz Buzz
+function fizzbuzz(n){
+  let solution = []
+  for(let i = 1 ; i <= n ; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+      solution.push('FizzBuzz')
+    }
+    else if ( i % 3 === 0){
+      solution.push('Fizz')
+    }
+    else if ( i % 5 === 0){
+      solution.push('Buzz')
+    }
+    else{
+      solution.push(i)
+    }
+  }
+  return solution
+}
