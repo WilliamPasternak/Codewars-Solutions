dae2765a8c1d9a0d71de2d83847e2edc351af48e
William Pasternak <wjp09@yahoo.com>
2022-08-07 00:36:24 -0400

    Last Challenge: Form The Minimum

--- 2. 7KYU/Arrays/Form The Minimum.js
+++ 2. 7KYU/Arrays/Form The Minimum.js
@@ -2,7 +2,7 @@ Form The Minimum
 Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
 
 function minValue(values){
-  let unique = [...new Set(values)]
+  let unique = Array.from(new Set(values))
   return Number(unique.sort().join(''))
 }
 
