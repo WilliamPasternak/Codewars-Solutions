0f479431719e1ad2c1fffc4d65ceb47840cb6363
William Pasternak <wjp09@yahoo.com>
2022-08-09 01:59:48 -0400

    Last Challenge: Greet Me

--- 2. 7KYU/Strings/Greet Me.js
+++ 2. 7KYU/Strings/Greet Me.js
@@ -1,4 +1,5 @@
 Greet Me
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
 var greet = function(name) {
   name = name.toLowerCase()
   return `Hello ${name[0].toUpperCase()}${name.substring(1)}!`
