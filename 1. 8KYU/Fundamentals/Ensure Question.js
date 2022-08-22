46432582ddedd0ef008c6711a931e98b44cf8a54
William Pasternak <wjp09@yahoo.com>
2022-08-22 18:21:54 -0500

    Last Challenge: Ensure Question

---
+++ 1. 8KYU/Fundamentals/Ensure Question.js
@@ -0,0 +1,3 @@
+Ensure question
+Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+const ensureQuestion = string => string.endsWith('?') ?  string : `${string}?`
