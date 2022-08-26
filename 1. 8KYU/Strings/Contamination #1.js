ba833f7fae33db6ec6c7a4d3b7849b5830fb16a2
William Pasternak <wjp09@yahoo.com>
2022-08-26 13:55:07 -0400

    Last Challenge: Contamination

--- 1. 8KYU/Strings/Contamination #1.js
+++ 1. 8KYU/Strings/Contamination #1.js
@@ -1,17 +1,6 @@
-Contamination #1 
 An AI has infected a text with a character!!
 This text is now fully mutated to this character.
 If the text or the character are empty, return an empty string.
 There will never be a case when both are empty as nothing is going on!!
 
-function contamination(text, char){
-  if(!text) return ''
-  if(!char) return ''
-  
-  let textArray = text.split('')
-  for(let i = 0; i < textArray.length; i++){
-    textArray[i] = char
-  }
-  
-  return textArray.join('')
-}
+let contamination = (text, char) => char.repeat(text.length)
