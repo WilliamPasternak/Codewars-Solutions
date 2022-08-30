31e8b6ace92bd8e369ff3cc91fa4245b7ab0b805
William Pasternak <wjp09@yahoo.com>
2022-08-30 11:11:06 -0400

    Last Challenge: Flatten

---
+++ 2. 7KYU/Fundamentals/Flatten.js
@@ -0,0 +1,6 @@
+Flatten
+Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+function flatten(array) {
+  return [].concat(...array);
+}
