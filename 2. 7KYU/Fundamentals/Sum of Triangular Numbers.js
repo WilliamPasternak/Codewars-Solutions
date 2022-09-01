6a2ead381f72d8183d2a90ecbd7d6b613a486501
William Pasternak <wjp09@yahoo.com>
2022-08-31 23:16:33 -0400

    Last Challenge: Sum of Trianglular Numbers

--- 2. 7KYU/Fundamentals/Sum of Triangular Numbers.js
+++ 2. 7KYU/Fundamentals/Sum of Triangular Numbers.js
@@ -1,7 +1,18 @@
-function sumTriangularNumbers(n) {
-  if (n < 0) {
-    return 0
-  } else {
-    return n * (n + 1) * (n + 2) / 6
+Return the sum of Triangular Numbers up-to-and-including the nth Triangular Number.
+
+  function sumTriangularNumbers(n) {
+  if(n < 0) return 0
+
+  let numsToSum = {1:1}
+  let triangleSum = 0
+
+  for(let i = 2; i <= n; i++){      
+    numsToSum[i] = i + numsToSum[i - 1]
   }
-}
+
+  for(let num in numsToSum){
+    triangleSum += numsToSum[num]
+  }
+ 
+  return triangleSum
+  }
