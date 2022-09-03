40e741c085c695bde358a3f7234f4d7de7e73960
William Pasternak <wjp09@yahoo.com>
2022-09-03 03:43:18 -0400

    “refactored”

--- 1. 8KYU/Strings/Remove all Exclamation Marks.js
+++ 1. 8KYU/Strings/Remove all Exclamation Marks.js
@@ -1,7 +1,10 @@
 Remove all exclamation marks 
-
 Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
 
 function remove (string) {
   return string.split('!').join('') + '!'
 }
+
+
+// Refactored to one-line
+let removeExclamations = (string) => string.split('!').join('') + '!'
