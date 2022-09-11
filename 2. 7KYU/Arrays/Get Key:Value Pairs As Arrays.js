077ceb04abe8bc8606124aadc635d3bb7b3bf6e7
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2022-09-11 12:33:03 -0400

    “refactored”

---
+++ 2. 7KYU/Arrays/Get Key:Value Pairs As Arrays.js
@@ -0,0 +1,6 @@
+Get key/value pairs as arrays
+Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+
+function keysAndValues(data){
+    return [Object.keys(data), Object.values(data)];
+}
