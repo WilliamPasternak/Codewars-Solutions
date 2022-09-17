c56f37e2725bdc771b5176436e74f846e3d58708
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2022-09-17 13:52:40 -0400

    Last Challenge: Nth Smallest Element

--- 2. 7KYU/Arrays/Nth Smallest Element.js
+++ 2. 7KYU/Arrays/Nth Smallest Element.js
@@ -1,7 +1,14 @@
 Nth Smallest Element 
 Given an array/list [] of integers , Find the Nth smallest element in this array of integers
 
+// Checks from back of array
 function nthSmallest(arr, pos){
- arr.sort((a,b) => b - a )
- return arr[arr.length - pos]
+let sorted = arr.sort((a,b)=> b-a);
+return sorted[arr.length-pos]
+}
+
+// Checks from front of array
+function nthSmallest(arr, pos){
+let sorted = arr.sort((a,b)=> a-b);
+return sorted[pos -1]
 }
