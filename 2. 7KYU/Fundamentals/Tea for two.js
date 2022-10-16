f4ab819e753a5bf16eddf39c18b6a20ff5357e41
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2022-10-16 18:45:18 -0400

    Last Challenge: Tea For Two

---
+++ 2. 7KYU/Fundamentals/Tea for two.js
@@ -0,0 +1,4 @@
+Tea for two
+Given a string or number write a function tea42 that takes as input a string or number and returns a string where every '2' charcter or digit has been replaced with a t.
+
+const tea42 = input => String(input).replaceAll('2','t');
