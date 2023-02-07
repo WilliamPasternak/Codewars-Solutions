3deddb192e6e510419f4c9438a0e516a1c1c2350
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2023-02-07 11:29:36 -0500

    Last Challenge: Are You Playing Banjo

---
+++ Python/1. 8KYU/Are You Playing Banjo?.py
@@ -0,0 +1,13 @@
+Are You Playing Banjo?
+
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+def are_you_playing_banjo(name):
+    plays_banjo = name[0].lower() == 'r'
+    if plays_banjo:
+        return name +' plays banjo'
+    else:
+        return name + ' does not play banjo'
