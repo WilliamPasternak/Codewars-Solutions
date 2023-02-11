427fb9ac91accfc9cce28bddab961980ec5c8512
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2023-02-11 16:08:03 -0500

    Last Challenge: Exes and Ohs

---
+++ Python/1. 8KYU/Exes and Ohs.py
@@ -0,0 +1,6 @@
+Exes and Ohs
+
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+def xo(s):
+    return s.count('x') == s.count('o')
