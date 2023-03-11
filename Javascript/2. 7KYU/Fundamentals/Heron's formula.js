1ae46d13b5345348a42fa9a15b60f2dc4c581d06
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2023-03-11 18:38:28 -0500

    Last Challenge: Heron's Formula

---
+++ Javascript/2. 7KYU/Fundamentals/Heron's formula.js
@@ -0,0 +1,8 @@
+Heron's formula
+
+Write function heron which calculates the area of a triangle with sides a, b, and c 
+
+function heron(a, b, c) {
+  const s = (a + b + c) / 2
+  return Math.sqrt(s * (s - a) * (s - b) * (s - c))
+}
