687edacd9bc7a962fd29bcbcc99ab5731004e5c6
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2023-03-15 22:29:25 -0400

    Last Challenge: Don't Rely on Luck

---
+++ Javascript/3. 6KYU/Fundamentals/Don't rely on luck.js
@@ -0,0 +1,13 @@
+Don't rely on luck.
+
+The test fixture I use for this kata is pre-populated.
+
+It will compare your guess to a random number generated using:
+
+Math.floor(Math.random() * 100 + 1)
+You can pass by relying on luck or skill but try not to rely on luck.
+
+"The power to define the situation is the ultimate power." - Jerry Rubin
+
+Math.random = () => 0 // 'Set' Math.random to = 0, so Math.floor(0 * 100 + 1) = 1
+let guess = 1
