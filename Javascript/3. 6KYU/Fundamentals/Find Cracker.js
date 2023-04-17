8e3c0a148a28dfec1e64ffad923d4b326c17cfc1
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2023-04-17 01:35:09 -0400

    “description”

--- Javascript/3. 6KYU/Fundamentals/Find Cracker.js
+++ Javascript/3. 6KYU/Fundamentals/Find Cracker.js
@@ -1,3 +1,16 @@
+Find Cracker
+Someone was hacking the score. Each student's record is given as an array The objects in the array are given again as an array of scores for each name and total score. 
+
+The scores for each grade is:
+A: 30 points
+B: 20 points
+C: 10 points
+D: 5 points
+Everything else: 0 points
+If there are 5 or more courses and all courses has a grade of B or above, additional 20 points are awarded. After all the calculations, the total score should be capped at 200 points.
+
+Return the name of the hacked name as an array when scoring with this rule.
+
 function findHack(arr) {
   const badKids = []
   const gradeScores = {'A':30,'B':20,'C':10,'D':5}
