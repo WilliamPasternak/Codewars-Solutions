14aaef214bc0bd0ba07c9683257684aafe26122a
William Pasternak <wjp09@yahoo.com>
2023-05-22 15:18:36 -0400

    Last Challenge: Unexpected Parsing

---
+++ Javascript/1. 8KYU/Fundamentals/Unexpected Parsing.js
@@ -0,0 +1,11 @@
+Unexpected parsing
+
+Function should return a dictionary/Object/Hash with "status" as a key, whose value can : "busy" or "available" depending on the truth value of the argument is_busy.
+
+But as you will see after clicking RUN or ATTEMPT this code has several bugs, please fix them.
+
+function getStatus(isBusy) {
+  let msg = (isBusy ? "busy" : "available");
+  const status = {status: msg}
+  return status
+}
