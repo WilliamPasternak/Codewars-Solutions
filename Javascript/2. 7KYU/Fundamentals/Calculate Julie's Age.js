bdd8796f301d5b386b437a62e0e470464e2b711b
William Pasternak <wjp09@yahoo.com>
2023-05-24 23:42:28 -0400

    Last Challenge: Calculate Julies Age

---
+++ Javascript/2. 7KYU/Fundamentals/Calculate Julie's Age.js
@@ -0,0 +1,8 @@
+Calculate Julie's Age
+Julie is x years older than her brother, and she is also y times as old as him.
+
+Given x and y calculate Julie's age using the function age(x, y).
+
+function age(x, y){
+  return x / (y - 1) *y
+}
