e6e8d774f0222ef6fb36099a4396af23eb52711c
William Pasternak <wjp09@yahoo.com>
2023-06-17 12:47:04 -0400

    Last Challenge: Waiting Room

---
+++ Javascript/2. 7KYU/Fundamentals/Waiting Room.js
@@ -0,0 +1,12 @@
+// Waiting Room
+// There's a waiting room with N chairs set in single row. Chairs are consecutively numbered from 1 to N. First is closest to the entrance (which is exit as well).
+
+// For some reason people choose a chair in the following way
+
+// Find a place as far from other people as possible
+// Find a place as close to exit as possible
+// All chairs must be occupied before the first person will be served
+
+function lastChair(n){
+ return n - 1
+}
