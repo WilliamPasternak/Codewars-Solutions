7ae82819712ed9b7df5ed2193756dd38d354c5b9
William Pasternak <wjp09@yahoo.com>
2023-08-27 15:41:25 -0400

    Last Challenge: Easy Logs

---
+++ Javascript/1. 8KYU/Fundamentals/easy logs.js
@@ -0,0 +1,7 @@
+easy logs
+
+Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+
+function logs(base, a, b) {
+  return Math.log(a * b) / Math.log(base);
+}
