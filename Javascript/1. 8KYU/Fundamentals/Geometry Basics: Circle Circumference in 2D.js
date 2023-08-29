9ed75d258bf4d72fbb4639ea3ced890d62ed6e67
William Pasternak <wjp09@yahoo.com>
2023-08-29 13:16:27 -0400

    Last Challenge: Circle Circumference

---
+++ Javascript/1. 8KYU/Fundamentals/Geometry Basics: Circle Circumference in 2D.js
@@ -0,0 +1,9 @@
+Geometry Basics: Circle Circumference in 2D
+
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Point objects have x, y attributes. Circle objects have center which is a Point, and radius, which is a number.
+
+Write a function calculating circumference of a Circle.
+
+const circleCircumference = circle => 2 * Math.PI  * circle.radius
