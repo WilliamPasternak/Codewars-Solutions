530c3bc03c3292bbd554f1e7c4a137d95fd4080d
William Pasternak <wjp09@yahoo.com>
2023-08-30 13:19:40 -0400

    Last Challenge: Circle Area

---
+++ Javascript/1. 8KYU/Fundamentals/Geometry Basics: Circle Area in 2D.js
@@ -0,0 +1,5 @@
+Geometry Basics: Circle Area in 2D
+
+Write the function circleArea/CircleArea which takes in a Circle object and calculates the area of that circle.
+
+const circleArea = circle => Math.PI * circle.radius**2
