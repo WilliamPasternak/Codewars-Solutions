ce8e2d4ab5b307979864113f950089dd6beaa800
William Pasternak <wjp09@yahoo.com>
2024-02-03 22:06:47 -0500

    Last Challenge: React Fun #1

---
+++ Javascript/3. 6KYU/Fundamentals/React Fun #1: Component types and nesting.js
@@ -0,0 +1,12 @@
+React Fun #1: Component types and nesting
+
+const React = require('react');
+
+const Hello = () => <h1>Hello</h1>
+const World = () => <h2>World!</h2>
+
+class Greet extends React.Component{
+  render(){
+    return <div><Hello /><World /></div>
+  }
+}
