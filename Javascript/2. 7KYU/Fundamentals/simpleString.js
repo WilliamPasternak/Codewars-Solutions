cb555c98b25d1e8de591425dc4eea083a97dc5b1
William Pasternak <wjp09@yahoo.com>
2024-10-20 23:56:29 -0400

    in progress

---
+++ Javascript/2. 7KYU/Fundamentals/simpleString.js
@@ -0,0 +1,23 @@
+function solve(characters){
+   let [uppercase, lowercase, numbers, specialCharacters] = [ 0, 0, 0, 0]
+   
+   // 65 - 90 Uppercase
+   // 48 - 57 nums
+   
+   
+   
+   for(let character of characters ) {
+     if (){
+       
+     }
+     else if (){
+
+     }
+     else if () {
+       
+     }
+     else if (){
+       
+     }
+   }
+}
