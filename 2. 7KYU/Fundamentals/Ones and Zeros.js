f8a9c568ba411fb6ca4a54cb8135992edff43801
William Pasternak <wjp09@yahoo.com>
2025-07-06 18:03:17 -0400

    “initial”

--- 2. 7KYU/Fundamentals/Ones and Zeros.js
+++ 2. 7KYU/Fundamentals/Ones and Zeros.js
@@ -17,3 +17,17 @@ for (let i = 0; i < arr.length ; i++){
 
 // Parse Int
 const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
+
+
+// For Each
+const binaryArrayToNumber = arr => {
+arr.reverse()
+let sum = 0
+let bin = 1
+
+arr.forEach(x => {
+  sum += x * bin    
+  bin *= 2  
+})
+  return sum 
+};
